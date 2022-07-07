d8388717f530fb22f6f2bb25bf6e910494ca89af
Bad-gamedev <108793674+Bad-gamedev@users.noreply.github.com>
2022-07-06 20:29:45 -0400

    Halfway complete addButtons

--- Setup.js
+++ Setup.js
@@ -1,1 +1,24 @@
 let world = [];
+for (let i = 0; i < 64; i++){
+
+//create row
+world[i] = [];
+
+//create columns
+for (let j = 0; j < 64; j++){
+
+world[i][j] = 0;
+
+}
+}
+
+let buttons = "";
+for (i = 0; i < 64; i++){
+buttons = buttons + '<button type="button" class="white"></button>\n'
+}
+
+function addButtons(){
+const div = document.createElement("div");
+div.innerHTML = buttons;
+document.getElementById("div").appendChild(div);
+}
